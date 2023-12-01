233731ae64648f203503616d400360054756bf38
Sayed Abdullah Qutb <s.abdullahqutb@gmail.com>
2023-12-01 19:29:44 +0000

    test heroku 5 added express server to host as static website

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
