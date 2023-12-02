424a1c149782e9a25977676aeb5e2688022e9394
Sayed Abdullah Qutb <s.abdullahqutb@gmail.com>
2023-12-02 13:03:49 +0000

    feat: added register page

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,113 @@
+<template>
+  <div
+    class="w-full flex flex-col justify-center items-center self-center my-16"
+  >
+    <Form
+      id="loginForm"
+      ref="loginForm"
+      v-slot="{ meta }"
+      class="flex flex-col w-3/4 sm:w-1/2 my-8 px-8 md:px-20 lg:px-40 py-12 shadow-2xl"
+    >
+      <h1 class="text-5xl text-center mb-8">
+        Register
+      </h1>
+      <label for="email">First Name</label>
+      <Field
+        id="name"
+        v-model:model-value="formData.name"
+        name="name"
+        rules="required|name"
+        as="input"
+        placeholder="First Name"
+        class="px-4 py-2 border border-gray-400 w-full min-h-[45px]"
+      >
+      </Field>
+      <small class="form-text text-red-400">
+        <ErrorMessage name="name" />
+      </small>
+      
+      <label class="mt-4" for="surname">Last Name</label>
+      <Field
+        id="surname"
+        v-model:model-value="formData.surname"
+        name="surname"
+        rules="required|name"
+        as="input"
+        placeholder="Last Name"
+        class="px-4 py-2 border border-gray-400 w-full min-h-[45px]"
+      >
+      </Field>
+      <small class="form-text text-red-400">
+        <ErrorMessage name="surname" />
+      </small>
+
+      <label class="mt-4" for="email">E-MAIL</label>
+      <Field
+        id="email"
+        v-model:model-value="formData.email"
+        name="email"
+        rules="required|email"
+        as="input"
+        type="email"
+        placeholder="Email"
+        class="px-4 py-2 border border-gray-400 w-full min-h-[45px]"
+      >
+      </Field>
+      <small class="form-text text-red-400">
+        <ErrorMessage name="email" />
+      </small>
+      
+      <label class="mt-4" for="password">Password</label>
+      <Field
+        id="password"
+        v-model:model-value="formData.password"
+        name="password"
+        rules="required|password"
+        as="input"
+        type="password"
+        placeholder="Password"
+        class="px-4 py-2 border border-gray-400 w-full min-h-[45px]"
+      >
+      </Field>
+      <small class="form-text text-red-400">
+        <ErrorMessage name="password" />
+      </small>
+      <button
+        class="btn btn-primary my-4"
+        @click="register"
+        :disabled="!meta.valid"
+      >
+        Register
+      </button>
+      <p>
+        Already have an account?
+      </p>
+      <router-link :to="{ name: 'LoginView' }">
+        <button class="w-full btn btn-secondary mt-2 my-4">Login</button>
+      </router-link>
+    </Form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+import { Form, Field, ErrorMessage } from 'vee-validate'
+
+const router = useRouter()
+const authStore = useAuthStore()
+
+const formData = ref({
+  name: '',
+  surname: '',
+  email: '',
+  password: ''
+})
+
+const register = async () => {
+  await authStore.register({ ...formData.value })
+  router.push({ name: 'Home' })
+}
+
+</script>
